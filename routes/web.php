6c4d7c01856118139544f0a2c6b12248002fba09
Jewel Osafile <osafilejewel@gmail.com>
2022-06-21 13:21:56 +0100

    update category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,5 +38,7 @@ Route::middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('add-category', 'Admin\CategoryController@add');
 
     Route::post('insert-category', 'Admin\CategoryController@insert');
+    Route::get('edit-prod/{id}', [CategoryController::class, 'edit']);
+    Route::put('update-category/{id}', [CategoryController::class, 'update']);
 });
  
