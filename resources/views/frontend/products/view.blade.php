5413c27d0957a6127d9c8d97c4431959fe66d266
Jewel Osafile <osafilejewel@gmail.com>
2022-06-23 13:51:17 +0100

    incre & decre quantity with jquery

--- resources/views/frontend/products/view.blade.php
+++ resources/views/frontend/products/view.blade.php
@@ -29,7 +29,7 @@
                 <label for="" class="me-3">Original Proce: <s>Rs {{$products->original_price}}</s></label>
                 <label for="" class="fw-bold">Original Proce: Rs {{$products->selling_price}}</label>
                 <p class="mt-3">
-                    {{!! $products->small_description}}
+                    {!! $products->small_description !!}
                 </p>
                 <hr>
                 @if ($products->qty > 0)
@@ -41,9 +41,9 @@
                     <div class="col-md-2">
                         <label for="Quantity">Quantity</label>
                         <div class="input-group text-center mb-3">
-                            <span class="input-group-text">-</span>
-                            <input type="text" name="quantity" value="1" class="form-control">
-                            <span class="input-group-text">+</span>
+                            <button class="input-group-text decrement-btn">-</button>
+                            <input type="text" name="quantity" value="1" class="form-control text-center qty-input">
+                            <button class="input-group-text increment-btn">+</button>
                         </div>
                     </div>
                     <div class="col-md-10">
@@ -54,7 +54,45 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-12">
+            <hr>
+            <h3>Description</h3>
+            <p class="mt-3">
+                {!! $products->description !!}
+            </p>
+        </div>
         </div>
     </div>
 </div>
 @endsection
+
+@section('scripts')
+<script>
+    $(document).ready(function (){
+        $('.increment-btn').click(function (e){
+            e.preventDefault();
+            var inc_value = $('.qty-input').val();
+            var value = parseInt(inc_value, 10);
+            value = isNaN(value) ? 0 : value;
+            if(value < 10)
+            {
+                value++;
+                $('.qty-input').val(value);
+            } 
+        })
+
+        $('.decrement-btn').click(function (e){
+            e.preventDefault();
+            var dec_value = $('.qty-input').val();
+            var value = parseInt(dec_value, 10);
+            value = isNaN(value) ? 0 : value;
+            if(value > 1)
+            {
+                value--;
+                $('.qty-input').val(value);
+            } 
+        })
+    })
+    </script>
+@endsection
+
