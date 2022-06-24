311df3fdeaf055050725d1e088ea34bbcabfb3ec
Jewel Osafile <osafilejewel@gmail.com>
2022-06-24 09:12:12 +0100

    stock validation in checkout page

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -38,21 +38,26 @@
                 </div>
                 <div class="col-md-3 my-auto" >
                     <input type="hidden" name="" class="prod_id" value="{{$item->prod_id}}">
+                        @if($item->products->qty > $item->prod_qty)
                     <label for="Quantity">Quantity</label>
                     <div class="input-group text-center mb-3" style="width: 130px;">
                         <button class="input-group-text changeQuantity decrement-btn">-</button>
                         <input type="text" name="quantity" class="form-control qty-input text-center" value="{{$item->prod_qty}}">
                         <button class="input-group-text changeQuantity increment-btn">+</button>
                     </div>
+                        @php
+                            $total += $item->products->selling_price * $item->prod_qty;
+                        @endphp
+                        @else
+                        <h6 class="text-danger">Out of stock</h6>
+                    @endif
                 </div>
                 <div class="col-md-2 my-auto">
                     <button class="btn btn-danger delete-cart-item"><i class="fa fa-trash"></i> Remove</button>
                 </div>
             </div>
             <hr>
-            @php
-                $total += $item->products->selling_price * $item->prod_qty;
-            @endphp
+            
             @endforeach
         </div>
         <div class="card-footer">
